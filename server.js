8277f214ffaf3b511084e2488de620c589185e13
cjj1120 <cjj8168@gmail.com>
2020-09-02 00:37:15 +0800

    Call data for Card plus decoration

--- server.js
+++ server.js
@@ -15,12 +15,40 @@ app.get('/contact', (req, res) => {
 app.use(express.static('Public'))
 
 app.get('/portfolio', (req, res) => {
-  res.render('portfolio')
+  res.render('portfolio', {projects: projectList})
 })
 
 app.get('/about', (req, res) => {
   res.render('about')
 })
+
+const projectList = [
+  {
+    logo: 'https://d2gg9evh47fn9z.cloudfront.net/800px_COLOURBOX29766346.jpg', 
+    title: 'Priority of life',
+    description: 'Having a work life balance is the upmost important',
+    way: 'Time management and Good habit',
+    category: 'lifestyle'
+
+  },
+  {
+    logo: 'https://d2gg9evh47fn9z.cloudfront.net/800px_COLOURBOX29766346.jpg', 
+    title: 'Second priority',
+    description: 'Having exercise everyday is important for both the mental and physical',
+    way: 'Hiking and Workout',
+    category: 'lifestyle'
+
+  },
+  {
+    logo: 'https://d2gg9evh47fn9z.cloudfront.net/800px_COLOURBOX29766346.jpg', 
+    title: 'Third priority',
+    description: 'Self reflection and self awareness is important for growth',
+    way: 'Journalizing and Introspection',
+    category: 'lifestyle'
+
+  }
+]
+
 
 app.listen(port, () => {
     console.log("Server is running on port: " + port)
